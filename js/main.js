b16db2518bbf2122cca9b139459e0316378d33af
rehanmo <rehan4295.rm@gmail.com>
2020-04-05 15:44:04 +0200

    cinerama template

--- js/main.js
+++ js/main.js
@@ -1,2 +1,50 @@
-var bleep = new Audio();
-  bleep.src = "bleep.mp3";
+var track = document.getElementById('track');
+
+var controlBtn = document.getElementById('play-pause');
+
+function playPause() {
+    if (track.paused) {
+        track.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else { 
+        track.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function() {
+  controlBtn.className = "play";
+});
+/*start fadout  */
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#div1").fadeOut();
+      $("#div2").fadeOut("slow");
+      $("#div3").fadeOut(3000);
+    });
+  });
+
+/*end fade out */
+/* start icon loading */
+
+$(window).on('load', function ()
+ {
+  $("body").css("overflow","auto"),
+  $(".loadoverlayicon .loadin2").fadeOut(1000,
+    
+    function()
+    {
+
+      $(this).parent().fadeOut(2000,
+        function(){
+          $(this).remove();
+        });
+    });
+});
+
+
+
+/*end icon loding */
